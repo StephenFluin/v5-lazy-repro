faed2790fe63a573887ff28584f98ccaab00818e
Stephen Fluin <stephen.fluin@gmail.com>
2017-10-24 17:14:50 -0700

    Repro issues with lazy loaded routes

---
+++ src/app/lazy/lazy.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LazyComponent } from './lazy/lazy.component';
+import { RouterModule } from '@angular/router';
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: '', component: LazyComponent },
+    ])
+  ],
+  declarations: [LazyComponent]
+})
+export class LazyModule { }
